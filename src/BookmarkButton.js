2f50f5efb93ee567dc8af79fbf756a152679bed3
jay nam <woalskdl@gmail.com>
2023-11-19 22:27:16 +0900

    useBookmark custom hook 추가

--- src/BookmarkButton.js
+++ src/BookmarkButton.js
@@ -1,15 +1,31 @@
 import { TouchableOpacity } from "react-native"
 import { Ionicons } from '@expo/vector-icons';
 import { COLOR } from './color';
+import { useState } from "react";
+
+const useBookmark = (initialIsBookmarked) => {
+  const [isBookmarked, setIsBookmarked] = useState(initialIsBookmarked);
+  const toggleIsBookmarked = () => setIsBookmarked(!isBookmarked);
+
+  return {
+    isBookmarked,
+    toggleIsBookmarked
+  }
+}
 
 export default ({
   size,
-  isBookmarked,
+  isBookmarked : isBookmarkedProp,
   onPress,
   style
 }) => {
+  const { isBookmarked, toggleIsBookmarked } = useBookmark(isBookmarkedProp);
+
   return (
-    <TouchableOpacity style={style} onPress={onPress}>
+    <TouchableOpacity style={style} onPress={() => {
+        toggleIsBookmarked();
+        onPress();
+      }}>
       <Ionicons
         name="star" 
         size={size} 
